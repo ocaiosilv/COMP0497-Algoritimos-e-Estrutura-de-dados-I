fe433ea0fae5f203914024de6cd903f34ce3ce05
Caio Aloisio <caio.aloisio@outlook.com>
2026-04-09 01:20:58 -0300

    Retirei os comentários

--- ListaSimples.java
+++ ListaSimples.java
@@ -2,19 +2,16 @@ class Node {
     public Object item; // dado armazenado (generico)
     public Node next;   // referencia ao proximo no
 
-    // TODO : crie um construtor Node ( Object item ) que
     public Node(Object x) {
         item = x;
         next = null;
     }
-    // inicializa this . item e coloca next = null
 
-    // TODO : crie um segundo construtor Node ( Object item , Node next )
     public Node(Object x, Node n) {
         item = x;
         next = n;
     }
-    // que permite ja encadear na criacao
+
 }
 
 public class ListaSimples {
@@ -44,28 +41,21 @@ public class ListaSimples {
     ou null se nao encontrar . */
     public Node find(Object item) {
         Node atual = head;  
-        // TODO : percorra a lista comparando atual . item com ’item ’
         while (atual != null) {
             if (atual.item.equals(item)){
                 return atual;
             }
             atual = atual.next;
         }
-        // Use . equals () para comparar objetos !
-        // Retorne ’atual ’ quando encontrar .
-        // Se chegar ao fim ( null ), retorne null .
         return null;
     }
 
     /** Insere um novo no com ’item ’ APOS o no ’x ’. */
     public void insertAfter(Node x, Object item) {
         if (x == null) return;
-        // TODO 1: crie um novo Node com ’item ’
         Node novoNode = new Node(item);
-        // TODO 2: faca novo . next = x. next
         novoNode.next = x.next;
         x.next = novoNode;
-        // TODO 3: faca x. next = novo
     }
 
     public static void main(String[] args) {
